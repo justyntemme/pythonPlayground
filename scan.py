661643c8fc27136cce46a69e20dd3620999aa21e
justyn temme <justyntemme@gmail.com>
2023-01-15 04:59:41 +0000

    add scan.py

---
+++ scan.py
@@ -0,0 +1,17 @@
+import subprocess
+import xmltodict
+from graphviz import Digraph
+
+def scan_network():
+    nmap_output = subprocess.run(["nmap", "-oX", "./sP", "192.168.12.0/24"], capture_output=True)
+    print(nmap_output.stdout)
+    nmap_data = xmltodict.parse(nmap_output.stdout.decode())
+    graph = Digraph()
+
+    for host in nmap_data['nmaprun']['host']:
+        hostname = host['hostnames']['hostname']['@name'] if host['hostnames'] is not None else host['address']['@addr']
+        graph.node(hostname)
+
+    graph.render('network_graph', view=True)
+
+scan_network()
